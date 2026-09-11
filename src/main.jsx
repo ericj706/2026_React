13cc26150b35e44953e041cec0cbb6363d649285
ericj706 <ericj6215@gmail.com>
2026-09-11 13:22:57 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+// 필수 1. 리액트 라이브러리 최초 렌더링 (그리기)하는 함수
+import { createRoot } from 'react-dom/client';
+// 필수 2. index.html에서 root마크업 가져오기, #ID, .Class
+const root = document.querySelector('#root')
+// 필수 3. 가져온 root 마크업을 createRoot함수에 전달한다.
+const create = createRoot(root);
+// [선택] 최초로 화면을 그리기 할 컴포넌트 가져와서 렌더링
+// 1. import 이용하여 컴포넌트 가져온다.
+// import App from './App/jsx'
+// 2. 가져온 컴포넌트 렌더링하기
+// create.render( <App> /<App> )
+
+// [day01] render 1번 가능하므로 예제 컴포넌트 변경
+// import MyMarkup from "./example/day01/MyMarkup";
+// create.render( <MyMarkup> /<MyMarkup> )
+
+// import Component1 from "./example/day01/Component1";
+// create.render( <Component> /<Component> )
+
+import Component2 from './example/day01/Component2';
+create.render( <Component2 />)
